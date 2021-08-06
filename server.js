0204f2b7903b13c9a01ed06e1703f53d074c3425
Fred <floony7@yahoo.com>
2021-08-07 08:43:32 +1200

    Init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http")
+const elements = require("./elements.json")
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" })
+  res.end(JSON.stringify(elements))
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`))
